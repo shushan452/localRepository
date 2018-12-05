bf83b30f57647027f11ae8c5e1242a5cfd11e6e8
wujiajia <318261776@qq.com>
2018-12-06 00:07:33 +0800

    myBatis-generator代码自动生成工具

---
+++ mybatis-generator-1.35/lib/src/com/shushan/entity/Student.java
@@ -0,0 +1,65 @@
+package com.shushan.entity;
+
+import java.util.Date;
+
+public class Student {
+    private Integer id;
+
+    private String code;
+
+    private String name;
+
+    private String phone;
+
+    private String address;
+
+    private Date birthday;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
